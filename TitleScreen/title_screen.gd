b86b6c5b897346efdaf3af6765888887ff15683f
JamesJamesJamesJamesJamesJames <67800776+JamesJamesJamesJamesJamesJames@users.noreply.github.com>
2023-11-11 22:02:02 -0500

    ADDED VOLUME SLIDER!!!!
    ADDED VLUME SLDIER JAMESJMAESJAMESJAMESJAMESJMAESJAMES

--- TitleScreen/title_screen.gd
+++ TitleScreen/title_screen.gd
@@ -6,15 +6,16 @@ func _ready():
 	$Node/Select2/Versus.disabled = true
 	$Node/Select2/Train.disabled = true
 	$Node/Select2/Back.disabled = true
-	
+	$OptionsMenu.hide();#lolol
 	$Title.modulate.a = 1;
 
 func _physics_process(delta):
+	if $OptionsMenu/Volume.value <= -20: $BGM.playing = false;
+	elif $BGM.playing == false: $BGM.playing = true;
+	$BGM.volume_db = $OptionsMenu/Volume.value;#IT'S JUST A VALUE
+	
 	if rotating == 1: $Title.modulate.a -= 2.8*delta
 	elif rotating == -1: $Title.modulate.a += 2.8*delta
-
-func _on_quit_pressed():
-	get_tree().quit()
 
 func _on_play_pressed():
 	var tween = get_tree().create_tween();
@@ -33,6 +34,18 @@ func _on_play_pressed():
 	await tween.finished;
 	rotating = 0;
 
+func _on_options_pressed():
+	#volume.
+	#so first off. so okay. so you want to have a volume slider
+	#
+	#	
+	$Node.hide();
+	$OptionsMenu.show();
+	pass # Replace with function body.
+
+func _on_quit_pressed():
+	get_tree().quit()
+
 func _on_back_pressed():
 	var backTween = get_tree().create_tween();
 	backTween.tween_property($Node, "rotation_degrees", 0, .5);
@@ -49,4 +62,7 @@ func _on_back_pressed():
 	await backTween.finished;
 	rotating = 0;
 
-
+func _on_options_back_pressed():
+	$Node.show();
+	$OptionsMenu.hide();
+	pass # Replace with function body.
