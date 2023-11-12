60e1a5cc63e2cdb296352fdb5a4ccbe35af40d85
Hospitalguy <rafe.sholler@gmail.com>
2023-11-11 23:43:48 -0500

    Changed camera and stage sizes

--- Characters/BaseCharacter.gd
+++ Characters/BaseCharacter.gd
@@ -6,10 +6,10 @@ class_name Fighter;
 @export var max_run_speed = 100.0;
 @export var air_speed = 1500.0;
 @export var max_air_speed = 100.0
+@export var jump_height = 150;
+
 var floored = true;
-
 var hasDoubleJump = false;
-
 var crouching = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -31,23 +31,23 @@ func _physics_process(delta):
 	#momvement controls
 	if floored:
 		hasDoubleJump = true;
-		if Input.is_action_pressed("move_left") and get_linear_velocity().x > -1*max_run_speed and !crouching:
-			apply_force(Vector2(-1*run_speed, 0));
+		if Input.is_action_pressed("move_left") and get_linear_velocity().x > -max_run_speed and !crouching:
+			apply_force(Vector2(-run_speed, 0));
 		if Input.is_action_pressed("move_right") and get_linear_velocity().x < max_run_speed and !crouching:
 			apply_force(Vector2(run_speed, 0));
 		if Input.is_action_just_pressed("jump"):
-			set_axis_velocity(Vector2(0, -300));
+			set_axis_velocity(Vector2(0, -jump_height));
 		if Input.is_action_pressed("crouch"):
 			crouching = true
 		if Input.is_action_just_released("crouch"):
 			crouching = false
 	else:
-		if Input.is_action_pressed("move_left") and get_linear_velocity().x > -1*max_air_speed:
-			apply_force(Vector2(-1*air_speed, 0));
+		if Input.is_action_pressed("move_left") and get_linear_velocity().x > -max_air_speed:
+			apply_force(Vector2(-air_speed, 0));
 		if Input.is_action_pressed("move_right") and get_linear_velocity().x < max_air_speed:
 			apply_force(Vector2(air_speed, 0));
 		if Input.is_action_just_pressed("jump") and hasDoubleJump:
-			set_axis_velocity(Vector2(0, -300));
+			set_axis_velocity(Vector2(0, -jump_height));
 			hasDoubleJump = false;
 		if Input.is_action_pressed("crouch"):
 			#fall twice as fast
