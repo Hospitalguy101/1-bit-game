70a355862cdc0056003cb62fb127d6d8fe3fb256
dirttop <gsyomichev@gmail.com>
2023-11-14 01:10:48 -0500

    Machamp Animations!
    yay

--- machamp.gd
+++ machamp.gd
@@ -8,6 +8,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if crouching:
+		$GPUParticles2D.set_position_(2,-7)
+	else:
+		$GPUParticles2D.set_position_(1,-11)
 	update_animation_param()
 
 func update_animation_param():
