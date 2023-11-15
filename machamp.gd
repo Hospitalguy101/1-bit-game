f7027810262555f62b743c2342e5b298a943c9e7
dirttop <gsyomichev@gmail.com>
2023-11-14 19:22:17 -0500

    Machamp Animations

--- machamp.gd
+++ machamp.gd
@@ -3,15 +3,15 @@ extends Fighter
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$BodyAnimation/BodyTree.active = true
+	$SArmAnim/SArmTree.active = true
+	$HArmAnim/HArmTree.active = true
+	$GArmAnim/GArmTree.active = true
+	$LArmAnim/LArmTree.active = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if crouching:
-		$GPUParticles2D.set_position_(2,-7)
-	else:
-		$GPUParticles2D.set_position_(1,-11)
 	update_animation_param()
 
 func update_animation_param():
