a8e2fe71e8e3b053f5cf0660d9c2c8d39b8bd255
Hospitalguy <rafe.sholler@gmail.com>
2023-11-17 23:22:39 -0500

    Updated title screen

--- game.gd
+++ game.gd
@@ -3,9 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Camera/UI/ready.visible = true
-	$ReadyTimer.start()
-	$Camera/UI/fight.visible = false
+	$Camera/UI/ready.visible = true;
+	$ReadyTimer.start();
+	$Camera/UI/fight.visible = false;
 
 
 func play_sfx(sfx):
@@ -17,16 +17,16 @@ func play_sfx(sfx):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Camera/UI/time.text = str(int($MatchTime.time_left))
+	$Camera/UI/time.text = str(int($MatchTime.time_left));
 
 func _on_ready_timer_timeout():
-	$Camera/UI/ready.visible = false
-	$Camera/UI/fight.visible = true
-	$GoTimer.start()
+	$Camera/UI/ready.visible = false;
+	$Camera/UI/fight.visible = true;
+	$GoTimer.start();
 
 func _on_go_timer_timeout():
-	$Camera/UI/fight.visible = false
-	$MatchTime.start()
+	$Camera/UI/fight.visible = false;
+	$MatchTime.start();
 
 func _on_match_time_timeout():
 	pass # Replace with function body.
